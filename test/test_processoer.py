6b6bb8fa1c95bfe97100cecd882b51008186fb5d
Yudai Udagawa <nimius.rd@gmail.com>
2017-05-21 13:43:37 +0900

    Add test in processer.extract_word

--- test/test_processoer.py
+++ test/test_processoer.py
@@ -13,3 +13,18 @@ class Processer(unittest.TestCase):
         for x in text:
             with self.subTest(x=x):
                 self.assertEqual(processer.remove_url(x), '')
+
+    def test_extract_word(self):
+        case = [
+            {
+                'text': 'これでSafari, Chrome  FirefoxでESMが使えるようになったのか',
+                'words': ['Safari', 'Chrome', 'Firefox', 'ESM']
+            },
+            {
+                'text': 'flake8, autoflake, autopepで運用しようか',
+                'words': ['flake8', 'autoflake', 'autopep']
+            },
+        ]
+        for x in case:
+            with self.subTest(x=x):
+                self.assertEqual(processer.extract_word(x['text']), x['words'])
